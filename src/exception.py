62622d789650ef998fa4299dddbc66f15b38aa9c
Emmanuel Nsubuga <cemputus019@gmail.com>
2024-08-24 09:56:46 +0300

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys 
+import logging
 
 
 
@@ -7,10 +8,10 @@ def error_message_detail(error, error_detail:sys):
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number[{1}] error message[{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
-    
+    )
     
     return error_message
-    )
+    
     
     
     
@@ -18,5 +19,16 @@ def error_message_detail(error, error_detail:sys):
     
 class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
+        
+        
+        
+    def __str__(self):
+        return self.error_message
+    
+    
+    
+    
+    
+    
